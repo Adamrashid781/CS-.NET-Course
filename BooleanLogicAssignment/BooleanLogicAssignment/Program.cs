9e51f96c1f0e1d870d702497bf946daa54e55f33
Adamrashid781 <86022836+Adamrashid781@users.noreply.github.com>
2024-06-05 19:34:21 -0700

    Boolean Asignemnt

---
+++ BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string inputAge = Console.ReadLine();
+            int age = Convert.ToInt32(inputAge);
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string DUI = Console.ReadLine();
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string inputTickets = Console.ReadLine();
+            int tickets = Convert.ToInt32(inputTickets);
+
+
+            Console.WriteLine(age + "\n" + DUI + "\n" + tickets);
+
+            if (age > 15 && DUI == "no" && tickets < 4) {
+                Console.WriteLine("You qualify for insurance with us!");
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
