d2fbafd6ac1e11b8827b46ffac570c3243c0ec47
Adamrashid781 <86022836+Adamrashid781@users.noreply.github.com>
2024-06-15 21:57:10 -0700

    Branching Assignment Push

---
+++ BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions.");
+            Console.WriteLine("Enter the package weight?");
+            int packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            int packageWidth, packageHeight, packageLength, packageDimensions;
+
+            if ( packageWeight < 50)
+            {
+                Console.WriteLine("What is the package width?");
+                packageWidth = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the package height");
+                packageHeight = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the packages length");
+                packageLength = Convert.ToInt32(Console.ReadLine());
+
+                packageDimensions = packageWidth + packageLength + packageHeight;
+                if (packageDimensions > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped VIA Package Express.");
+                }
+                else
+                {
+                    int shippingCost = (packageWeight * packageHeight * packageWidth * packageLength) / 100;
+
+                    Console.WriteLine("Your total cost for shipping is: " + shippingCost.ToString("C"));
+                }
+            }
+
+
+            else
+            {
+                Console.WriteLine("Package is too heavy to be shipped VIA Package Express. Have a good day.");
+
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IfChallenges/IfChallenges/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "adam";
+
+            //if (name == "Jim")
+            //{
+            //    Console.WriteLine("Your name is Jim");
+            //}
+            //else if (name == "Tom")
+            //{
+            //    Console.WriteLine("Your name is Tom");
+            //}
+            //else if (name != "Ayia")
+            //{
+            //    Console.WriteLine("You are happy");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Your name is not Jim or Tom. Who TF are you");
+            //}
+
+            Console.WriteLine("Ternary Operators below:");
+
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            int currentTemp = 80;
+            Console.WriteLine("Hi, " + name + " What is the room temp?");
+            int roomTemp = Convert.ToInt32(Console.ReadLine());
+
+            string comparison = currentTemp == roomTemp ? "It is room temp" : "It is not room temp";
+            Console.WriteLine(comparison);
+
+            
+
+            
+
+            string answer = 5 > 2 ? "Num 1 is greater than num 2" : "num 2 is greater than num 1 ";
+            Console.WriteLine(answer);
+
+            string names = name != "adam" ? "your name is adam" : "Name is not adam ";
+            Console.WriteLine(names);
+
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
