080971df000d5b3cfdb7d082b6b23365cced7cae
Adamrashid781 <86022836+Adamrashid781@users.noreply.github.com>
2024-06-18 02:46:16 -0700

    strings and integers assignment

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number");
+                int number1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Pick another number");
+                int number2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Dividing the two...");
+
+                int numberThree = number1 / number2;
+
+                Console.WriteLine(number1 + " Divided by " + number2 + " equals: " + numberThree);
+                Console.ReadLine();
+            }
+            // You can put as many catch blocks as youd like and only the ones that apply to the error made will execute
+            // But even if there is no error, the Finally catch block will execute regardless
+            // ex is of datatype FormatException
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please dont divide by zero");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Usually a database Log will be in here, for passes and fails
+                Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
---
+++ StringsAndIntegersAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Creating the list that has random number in it
+                List<int> numList = new List<int>() { 2, 4, 5, 8, 22, 99, 90, 0 };
+
+                // Displaying the numbers with a simple foreach loop
+                foreach (int num in numList)
+                {
+                    Console.WriteLine(num);
+                }
+
+                // Taking the user input asa float
+                Console.WriteLine("\n\nInput a number to divide all the number in the list by:");
+
+                // Using the float.Parse() function to take in a user input that is a flaot
+                // If user enters a Zero, it will not throw a error, in order to correct that, there needs
+                // to be some if statements to catch this
+                float userInput = float.Parse(Console.ReadLine());
+
+                // creating the variable to assign the result to
+                float x = 0;
+
+                // This foreach loop divides the numbers in the list one at a time by the userInput variable
+                // and assigns it to x. 
+                //Using the (dataType)Math.Round(var, 2) function to round the sum to 2 decimal places
+                foreach (int num in numList)
+                {
+                    x = num / userInput;
+                    x = (float)Math.Round(x, 2);
+                    // This if statement will tell the user that they are dividing by zero and equal infinity
+                    if (float.IsInfinity(x))
+                    {
+                        Console.WriteLine(num + " / " + userInput + " Result is infinity, do not input a Zero.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(num + " / " + userInput + " Equals: " + x);
+                    }
+                   
+                }
+            }
+
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program executed and exited the catch blocks.");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
