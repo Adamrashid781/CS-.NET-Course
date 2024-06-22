b933d8d0c36d4825cb414cddc5387a4d7f236fbd
Adamrashid781 <86022836+Adamrashid781@users.noreply.github.com>
2024-06-21 23:31:34 -0700

    Methods and Objects Assignment

--- 21CardGame/21CardGame/Program.cs
+++ 21CardGame/21CardGame/Program.cs
@@ -27,21 +27,29 @@ namespace _21CardGame
 
 
 
-            // Creating a object of data type Deck
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Adam", "Joe" };
+            game.ListPlayers();
+            game.Play();
+            Console.ReadLine();
+            game.Dealer = "Jesse";
+            game.Name = "TwentyOne";
 
-            Deck deck = new Deck();
-            // deck = Shuffle(deck);
+
+            //// Creating a object of data type Deck
+            //Deck deck = new Deck();
             
-            deck.Shuffle( 5);
-            // you can put named perameters
-            // deck = Shuffle(deck: deck, times: 5);
+            
+            //deck.Shuffle( 5);
+            //// you can put named perameters
+            //// deck = Shuffle(deck: deck, times: 5);
 
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
 
             
 
---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+
+
+            person.FirstName = "Adam";
+            person.LastName = "Rashid";
+
+            person.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
